136bfc553627ea032bac6422513b79454cf6f8da
Tom Ramsden <thomas.ramsden20@gmail.com>
2021-07-15 15:16:47 +0100

    Added setSail function and changed over to class constructor rather than using prototype

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -3,22 +3,37 @@ const Ship = require('../src/ship');
 describe("constructor", () => {
     let ship;
     beforeEach(() => {
-        ship = new Ship("Serenity", "Dover");
-    })
+        ship = new Ship("Highwind", "Junon");
+    });
 
     it("returns an object", () => {
         expect(ship).toBeInstanceOf(Object);
     });
 
     it("sets the name and startingPort property", () => {
-        expect(ship).toEqual({"name": "Serenity", "startingPort": "Dover"});
+        expect(ship).toEqual({"name": "Highwind", "startingPort": "Junon"});
     });
 
     it("checks a starting port has been assigned", () => {
-        expect(ship.startingPort).toBe("Dover");
-    })
+        expect(ship.startingPort).toBe("Junon");
+    });
 
     it("returns an error if arguments are not passed", () => {
-        expect(() => new Ship()).toThrow("Please name the ship and the starting port.");
+        expect(() => new Ship()).toThrow(Error);
+    });
+});
+
+describe("setSail", () => {
+    let ship;
+    beforeEach(() => {
+        ship = new Ship("Highwind", "Junon");
+    });
+
+    it("gets the ship started on its journey", () => {
+        expect(ship.setSail()).toBe("Highwind has departed from Junon.");
+    });
+
+    it("expects startingPoint to be falsy after setSail() is executed", () => {
+        expect(ship.startingPort).toBeFalsy();
     });
 });
--- src/ship.js
+++ src/ship.js
@@ -1,10 +1,16 @@
-function Ship(name, startingPort) {
-    if (name === undefined || startingPort === undefined) {
-        throw new Error("Please name the ship and the starting port.")
-    } else {
-        this.name = name;
-        this.startingPort = startingPort;
+class Ship {
+    constructor(name, startingPort) {
+        if (name === undefined || startingPort === undefined) {
+            throw new Error("Please give the ship a name and a starting port.")
+        } else {
+            this.name = name;
+            this.startingPort = startingPort;
+        }
     }
-}
+
+    setSail() {
+        return `${this.name} has departed from ${this.startingPort}.`
+    }
+};
 
 module.exports = Ship;
